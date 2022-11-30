0dba59d97938524db062e56071bd688c49adf36a
Dake <example@gmail.com>
2022-11-30 21:03:28 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,169 @@
+"use strict";
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+// 	a: 50,
+// 	b: 100
+// };
+
+// obj.a = 20;
+// console.log(obj);
+
+// console.log(name);
+
+// var name = 'Ivan';
+
+// {
+// 	var result = 60;
+// }
+// console.log(result);
+
+// a = 15;
+// console.log(a);
+
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log('Ширина кузова :' + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+
+
+
+// Классификация типов данных в JavaScript
+
+
+// let number = 4.6;
+// console.log(4 / 0);
+// console.log('string' * 9);
+
+// const person = 'Alex';
+// console.log(person );
+
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+// 	name: 'John',
+// 	age: 25,
+// 	isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+//  ------ 12 Разница между объектами и массивами и неочевидные синтаксические возможности------//
+
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '3456';
+// console.log(arr);
+
+
+// const arrObj = {
+// 	'0': 'a',
+// 	1: 'b',
+// 	2: 'c',
+// 	abc: {
+// 		df:[{}, {}],
+// 		def: {
+
+// 		}
+// 	}
+// };
+
+// const b = 'b';
+
+// arrObj[b] = {};
+
+
+
+// console.log(arrObj);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+// const obj = { a: 1, b: 2 };
+
+// const obj = {
+// 	Anna: 500,
+// 	'Alice': 800
+// };
+
+
+// Просто общение с пользователем
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '');
+// console.log(answer+10);
+
+// const answers = [];
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваше фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+
+
+//14. Интерполяция (ES6)
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Привет ${user}`);
+
+
+
+// 15 Операторы JS
+
+// console.log('arr'+ ' - object');
+// console.log(4 + ' - object');
+// console.log(4 + +'5');
+
+let incr = 10,
+	decr = 10;
+
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log((2 + 2) * 2 != 8);
+
+const isChecked = false,
+	isClose = false;
+
+// console.log(isChecked && isClose);
+console.log(isChecked || !isClose);
+
+
+
+
+
+
+
+
+
+
+
+
